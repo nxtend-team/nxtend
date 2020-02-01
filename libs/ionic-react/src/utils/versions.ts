450898d31a4da2f153258c47f60486ad7510b99d
Devin Shoemaker <devinshoe@gmail.com>
2020-01-31 18:05:19 -0600

    feat(ionic-react): port versions.ts from @nrwl/react

---
+++ libs/ionic-react/src/utils/versions.ts
@@ -0,0 +1,28 @@
+export const nxVersion = '*';
+
+export const reactVersion = '16.12.0';
+export const reactDomVersion = '16.12.0';
+export const typesReactVersion = '16.9.17';
+export const typesReactDomVersion = '16.9.4';
+
+export const styledComponentsVersion = '5.0.0';
+export const typesStyledComponentsVersion = '4.4.2';
+
+export const emotionStyledVersion = '10.0.27';
+export const emotionCoreVersion = '10.0.27';
+
+export const reactRouterDomVersion = '5.1.2';
+export const typesReactRouterDomVersion = '5.1.3';
+
+export const testingLibraryReactVersion = '9.4.0';
+
+export const babelPresetReactVersion = '7.8.3';
+
+export const reduxjsToolkitVersion = '1.1.0';
+export const reactReduxVersion = '7.1.3';
+export const typesReactReduxVersion = '7.1.5';
+
+export const eslintPluginImportVersion = '2.19.1';
+export const eslintPluginJsxA11yVersion = '6.2.3';
+export const eslintPluginReactVersion = '7.17.0';
+export const eslintPluginReactHooksVersion = '2.3.0';
