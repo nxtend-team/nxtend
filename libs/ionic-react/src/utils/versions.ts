87243397f2757130b2718b9a640428bb52066718
Devin Shoemaker <devinshoe@gmail.com>
2020-02-01 12:26:01 -0600

    fix(ionic-react): match dependency versions with Ionic React base
    https://github.com/ionic-team/starters/tree/master/react/base

--- libs/ionic-react/src/utils/versions.ts
+++ libs/ionic-react/src/utils/versions.ts
@@ -29,8 +29,8 @@ export const eslintPluginReactHooksVersion = '2.3.0';
 
 export const nxtendVersion = '0.0.1';
 
-export const ionicReactVersion = '4.11.10';
-export const ioniconsVersion = '4.6.3';
+export const ionicReactVersion = '^4.11.8';
+export const ioniconsVersion = '^4.6.3';
 
-export const testingLibraryJestDomVersion = '5.1.0';
-export const testingLibraryUserEventVersion = '8.1.0';
+export const testingLibraryJestDomVersion = '^4.2.4';
+export const testingLibraryUserEventVersion = '^8.0.3';
