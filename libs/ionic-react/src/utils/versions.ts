ef7bc25d8e260d4a67f925121c7e7d49af1103a3
Devin Shoemaker <devinshoe@gmail.com>
2020-02-16 10:47:46 -0600

    chore(ionic-react): upgrade Ionic to v5

--- libs/ionic-react/src/utils/versions.ts
+++ libs/ionic-react/src/utils/versions.ts
@@ -1,8 +1,8 @@
 export const nxtendVersion = '0.0.1';
 
-export const ionicReactVersion = '^4.11.8';
-export const ionicReactRouterVersion = '^4.11.8';
-export const ioniconsVersion = '^4.6.3';
+export const ionicReactVersion = '^5.0.0';
+export const ionicReactRouterVersion = '^5.0.0';
+export const ioniconsVersion = '^5.0.0';
 
 export const testingLibraryJestDomVersion = '^4.2.4';
-export const testingLibraryUserEventVersion = '^8.0.3';
+export const testingLibraryUserEventVersion = '^8.1.0';
