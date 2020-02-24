3509415acd04fe5b08df699c6981292ab4ed313f
Devin Shoemaker <devinshoe@gmail.com>
2020-02-24 13:36:50 -0600

    chore(ionic-react): upgrade @testing-library/user-event to 8.1.3

--- libs/ionic-react/src/utils/versions.ts
+++ libs/ionic-react/src/utils/versions.ts
@@ -5,4 +5,4 @@ export const ionicReactRouterVersion = '^5.0.1';
 export const ioniconsVersion = '^5.0.0';
 
 export const testingLibraryJestDomVersion = '^4.2.4';
-export const testingLibraryUserEventVersion = '^8.1.0';
+export const testingLibraryUserEventVersion = '^8.1.3';
