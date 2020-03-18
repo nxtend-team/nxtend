b5a422d1ab9ba22555f1bdc2e1e53f68c6b1bc3d
Devin Shoemaker <devinshoe@gmail.com>
2020-03-17 21:22:56 -0500

    cleanup(ionic-react): use exact versions when adding dependencies

--- libs/ionic-react/src/utils/versions.ts
+++ libs/ionic-react/src/utils/versions.ts
@@ -1,9 +1,9 @@
 export const nxtendVersion = '*';
 
-export const ionicReactVersion = '^5.0.5';
-export const ionicReactRouterVersion = '^5.0.5';
-export const ioniconsVersion = '^5.0.0';
+export const ionicReactVersion = '5.0.5';
+export const ionicReactRouterVersion = '5.0.5';
+export const ioniconsVersion = '5.0.0';
 
-export const testingLibraryJestDomVersion = '^4.2.4';
-export const testingLibraryUserEventVersion = '^8.1.3';
-export const testingLibraryCypressVersion = '^5.3.0';
+export const testingLibraryJestDomVersion = '4.2.4';
+export const testingLibraryUserEventVersion = '8.1.3';
+export const testingLibraryCypressVersion = '5.3.0';
