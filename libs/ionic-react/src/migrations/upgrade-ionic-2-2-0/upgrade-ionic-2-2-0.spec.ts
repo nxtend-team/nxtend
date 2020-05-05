5e8bf6c57e40beec17a75f067e947b5379c43540
Devin Shoemaker <devinshoe@gmail.com>
2020-05-04 20:55:48 -0500

    chore(ionic-react): upgrade Ionic to 5.1.0

---
+++ libs/ionic-react/src/migrations/upgrade-ionic-2-2-0/upgrade-ionic-2-2-0.spec.ts
@@ -0,0 +1,39 @@
+import { Tree } from '@angular-devkit/schematics';
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { readJsonInTree, serializeJson } from '@nrwl/workspace';
+import { createEmptyWorkspace } from '@nrwl/workspace/testing';
+import * as path from 'path';
+
+describe('Update 2.2.0', () => {
+  let initialTree: Tree;
+  let schematicRunner: SchematicTestRunner;
+
+  beforeEach(async () => {
+    initialTree = createEmptyWorkspace(Tree.empty());
+
+    schematicRunner = new SchematicTestRunner(
+      '@nxtend/ionic-react',
+      path.join(__dirname, '../../../migrations.json')
+    );
+
+    initialTree.overwrite(
+      'package.json',
+      serializeJson({
+        dependencies: {
+          '@ionic/react': '5.0.7',
+          '@ionic/react-router': '5.0.7',
+        },
+      })
+    );
+  });
+
+  it(`should update Ionic to 5.1.0`, async () => {
+    const result = await schematicRunner
+      .runSchematicAsync('upgrade-ionic-2.2.0', {}, initialTree)
+      .toPromise();
+
+    const { dependencies } = readJsonInTree(result, '/package.json');
+    expect(dependencies['@ionic/react']).toEqual('5.1.0');
+    expect(dependencies['@ionic/react-router']).toEqual('5.1.0');
+  });
+});
