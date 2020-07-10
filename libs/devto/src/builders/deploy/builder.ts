5aa6316ebc06de1d7ba2bbbcd43f0ace6a609fd6
Devin Shoemaker <devinshoe@gmail.com>
2020-07-10 11:34:54 -0500

    improve builder error handling

--- libs/devto/src/builders/deploy/builder.ts
+++ libs/devto/src/builders/deploy/builder.ts
@@ -1,12 +1,13 @@
 import {
   BuilderContext,
   BuilderOutput,
+  BuilderRun,
   createBuilder,
 } from '@angular-devkit/architect';
 import { NodeJsSyncHost } from '@angular-devkit/core/node';
 import { join } from 'path';
-import { from, Observable, of } from 'rxjs';
-import { mergeMap } from 'rxjs/operators';
+import { forkJoin, from, Observable, of, throwError } from 'rxjs';
+import { catchError, first, switchMap } from 'rxjs/operators';
 import { getProjectRoot } from '../../utils/project-root';
 import { DeployBuilderSchema } from './schema';
 
@@ -17,23 +18,81 @@ export function runBuilder(
   const host = new NodeJsSyncHost();
 
   return from(getProjectRoot(context, host)).pipe(
-    mergeMap((sourceRoot) => {
-      return context.scheduleBuilder('@nrwl/workspace:run-commands', {
-        cwd: join(context.workspaceRoot, sourceRoot),
-        commands: [
-          {
-            command: `node ${context.workspaceRoot}/node_modules/.bin/prettier \"**/*.{js,json,scss,md,ts,html,component.html}\" --list-different`,
-          },
-          {
-            command: `node ${context.workspaceRoot}/node_modules/.bin/embedme blog-posts/**/*.md`,
-          },
-          {
-            command: `node ${context.workspaceRoot}/node_modules/.bin/dev-to-git`,
-          },
-        ],
+    switchMap((sourceRoot) => {
+      return forkJoin({
+        sourceRoot: of(sourceRoot),
+        builderRun: from(
+          context.scheduleBuilder('@nrwl/workspace:run-commands', {
+            cwd: join(context.workspaceRoot, sourceRoot),
+            parallel: false,
+            commands: [
+              {
+                command: `node ${context.workspaceRoot}/node_modules/.bin/prettier "**/*.{js,json,scss,md,ts,html,component.html}" --list-different`,
+              },
+            ],
+          })
+        ),
       });
     }),
-    mergeMap((run) => run.output)
+    first(),
+    switchMap((runnerWithRoot) => {
+      return forkJoin({
+        sourceRoot: of(runnerWithRoot.sourceRoot),
+        builderOutput: runnerWithRoot.builderRun.output,
+      });
+    }),
+    switchMap((outputWithRoot) => {
+      if (outputWithRoot.builderOutput.success) {
+        return forkJoin({
+          sourceRoot: of(outputWithRoot.sourceRoot),
+          builderRun: from(
+            context.scheduleBuilder('@nrwl/workspace:run-commands', {
+              cwd: join(context.workspaceRoot, outputWithRoot.sourceRoot),
+              parallel: false,
+              commands: [
+                {
+                  command: `node ${context.workspaceRoot}/node_modules/.bin/embedme blog-posts/**/*.md`,
+                },
+              ],
+            })
+          ),
+        });
+      } else {
+        return throwError(outputWithRoot.builderOutput);
+      }
+    }),
+    switchMap((runnerWithRoot) => {
+      return forkJoin({
+        sourceRoot: of(runnerWithRoot.sourceRoot),
+        builderOutput: runnerWithRoot.builderRun.output,
+      });
+    }),
+    switchMap((outputWithRoot) => {
+      if (outputWithRoot.builderOutput.success) {
+        return context.scheduleBuilder('@nrwl/workspace:run-commands', {
+          cwd: join(context.workspaceRoot, outputWithRoot.sourceRoot),
+          parallel: false,
+          commands: [
+            {
+              command: `node ${context.workspaceRoot}/node_modules/.bin/dev-to-git`,
+            },
+          ],
+        });
+      } else {
+        return throwError(outputWithRoot.builderOutput);
+      }
+    }),
+    switchMap((builderRun: BuilderRun) => {
+      return builderRun.output;
+    }),
+    switchMap((builderOutput: BuilderOutput) => {
+      if (builderOutput.success) {
+        return of(builderOutput);
+      } else {
+        return throwError(builderOutput);
+      }
+    }),
+    catchError((builderOutput: BuilderOutput) => of(builderOutput))
   );
 }
 
