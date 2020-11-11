62d676b500503595ac7253d0a329cf5b9538f846
Devin Shoemaker <devinshoe@gmail.com>
2020-11-11 10:15:20 -0600

    chore(ionic-react): initialize plugin with @nxtend/capacitor 2.0.2 (#294)

--- packages/ionic-react/src/utils/versions.ts
+++ packages/ionic-react/src/utils/versions.ts
@@ -1,4 +1,4 @@
-export const nxtendCapacitorVersion = '1.1.0';
+export const nxtendCapacitorVersion = '2.0.2';
 
 export const ionicReactVersion = '5.4.1';
 export const ionicReactRouterVersion = '5.4.1';
