e7b4ccd4db4957f14ba36511e23b4a34dae3b097
Devin Shoemaker <devinshoe@gmail.com>
2020-11-17 09:18:13 -0600

    fix(ionic-react): generate `__mocks__/` directory under `src/`
    The `__mocks__/` directory was previously being generated under `src/app/`. This can cause issues when generating React components with Nx due to the fact that they are generated under `src/` by default.

--- packages/ionic-react/src/schematics/application/schematic.spec.ts
+++ packages/ionic-react/src/schematics/application/schematic.spec.ts
@@ -52,7 +52,7 @@ describe('application', () => {
       expect(tree.exists(`${projectRoot}/jest.config.js`)).toBeTruthy();
       expect(tree.exists(`${projectRoot}/src/test-setup.ts`)).toBeTruthy();
       expect(
-        tree.exists(`${projectRoot}/src/app/__mocks__/fileMock.js`)
+        tree.exists(`${projectRoot}/src/__mocks__/fileMock.js`)
       ).toBeTruthy();
     } else if (options.unitTestRunner === 'none') {
       expect(tree.exists(`${projectRoot}/jest.config.js`)).toBeFalsy();
