9ef0b670ea4540e535f420d0c4c7fad14e38c0e0
Devin Shoemaker <devinshoe@gmail.com>
2020-12-05 10:56:12 -0600

    chore(ionic-react): update Ionic to 5.5.1 (#324)

--- packages/ionic-react/src/utils/versions.ts
+++ packages/ionic-react/src/utils/versions.ts
@@ -1,5 +1,5 @@
 export const nxtendCapacitorVersion = '2.0.2';
 
-export const ionicReactVersion = '5.4.1';
-export const ionicReactRouterVersion = '5.4.1';
-export const ioniconsVersion = '5.1.2';
+export const ionicReactVersion = '^5.5.1';
+export const ionicReactRouterVersion = '^5.5.1';
+export const ioniconsVersion = '^5.2.3';
