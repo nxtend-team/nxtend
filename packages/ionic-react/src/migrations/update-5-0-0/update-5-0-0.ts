9b7a2e6fa86b7048e98c0cda4eaa9c4082817013
Devin Shoemaker <devinshoe@gmail.com>
2020-12-06 12:20:30 -0600

    wip

--- packages/ionic-react/src/migrations/update-5-0-0/update-5-0-0.ts
+++ packages/ionic-react/src/migrations/update-5-0-0/update-5-0-0.ts
@@ -1,9 +1,11 @@
 import { chain, Rule } from '@angular-devkit/schematics';
 import { updatePackagesInPackageJson } from '@nrwl/workspace';
 import * as path from 'path';
+import { addUpdateTask } from '../../utils/update-task';
 
 export default function update(): Rule {
   return chain([
+    addUpdateTask('@nxtend/capacitor', '2.2.0'),
     updatePackagesInPackageJson(
       path.join(__dirname, '../../../', 'migrations.json'),
       '5.0.0'
