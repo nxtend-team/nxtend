488dc4a2805b37bf126303c415e14849423a2f65
Devin Shoemaker <devinshoe@gmail.com>
2020-12-11 12:03:13 -0600

    chore(ionic-angular): standardize template (#332)

--- packages/ionic-angular/src/schematics/application/lib/files.ts
+++ packages/ionic-angular/src/schematics/application/lib/files.ts
@@ -1,9 +1,11 @@
 import {
   apply,
   applyTemplates,
+  chain,
   MergeStrategy,
   mergeWith,
   move,
+  noop,
   Rule,
   Tree,
   url,
@@ -11,9 +13,9 @@ import {
 import { names } from '@nrwl/workspace';
 import { NormalizedSchema } from '../schema';
 
-export function addFiles(options: NormalizedSchema): Rule {
+function addBaseTemplateFiles(options: NormalizedSchema): Rule {
   return mergeWith(
-    apply(url(`./files`), [
+    apply(url(`./files/base`), [
       applyTemplates({
         ...options,
         ...names(options.name),
@@ -22,6 +24,23 @@ export function addFiles(options: NormalizedSchema): Rule {
     ]),
     MergeStrategy.Overwrite
   );
+}
+
+function addBlankTemplateFiles(options: NormalizedSchema): Rule {
+  return mergeWith(
+    apply(url(`./files/blank`), [
+      applyTemplates({
+        ...options,
+        ...names(options.name),
+      }),
+      move(options.appProjectRoot),
+    ]),
+    MergeStrategy.Overwrite
+  );
+}
+
+export function addFiles(options: NormalizedSchema): Rule {
+  return chain([addBaseTemplateFiles(options), addBlankTemplateFiles(options)]);
 }
 
 export function removeFiles(options: NormalizedSchema): Rule {
