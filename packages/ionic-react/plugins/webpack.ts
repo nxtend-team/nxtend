42b5434590373ca5df48067414876667bdc99832
Devin Shoemaker <devinshoe@gmail.com>
2020-12-29 20:51:29 -0600

    chore(ionic-react): simplify Webpack implementation (#359)
    It does not appear that the Webpack config modifications are necessary with recent versions of Nx. The Nxtend plugin will still be added to the `workspace.json` to make plugin migrations reliable.

--- packages/ionic-react/plugins/webpack.ts
+++ packages/ionic-react/plugins/webpack.ts
@@ -3,13 +3,6 @@ const getWebpackConfig = require('@nrwl/react/plugins/webpack');
 
 function getCustomWebpackConfig(webpackConfig) {
   const config = getWebpackConfig(webpackConfig);
-
-  // Polyfill Node
-  config.node = {
-    global: true,
-    process: true,
-  };
-
   return config;
 }
 
