29c2a231e37387d237f9883504d873c59ded3c33
Devin Shoemaker <devinshoe@gmail.com>
2021-01-01 23:22:22 -0600

    chore(ionic-react): remove custom Webpack config

--- packages/ionic-react/plugins/webpack.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const getWebpackConfig = require('@nrwl/react/plugins/webpack');
-
-function getCustomWebpackConfig(webpackConfig) {
-  const config = getWebpackConfig(webpackConfig);
-  return config;
-}
-
-module.exports = getCustomWebpackConfig;
