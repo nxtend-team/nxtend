9dcdedb4c1b36996d56b0d03c91b848a464738e2
Devin Shoemaker <devinshoe@gmail.com>
2021-01-04 20:59:07 -0600

    release(core): Nxtend 11 release(ionic-react/ionic-angular): Nxtend 11

--- packages/ionic-angular/src/utils/versions.ts
+++ packages/ionic-angular/src/utils/versions.ts
@@ -1,3 +1,3 @@
-export const nxtendCapacitorVersion = '2.0.2';
+export const nxtendCapacitorVersion = '^11.0.0';
 export const ionicAngularVersion = '^5.5.2';
 export const ionicNativeVersion = '^5.30.0';
