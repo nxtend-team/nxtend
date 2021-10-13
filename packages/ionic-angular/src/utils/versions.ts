b78d47c4fa33ce2b0f5e0911793ffaf4fa206f36
Devin Shoemaker <devinshoe@gmail.com>
2021-10-13 08:24:23 -0500

    release(core): 12.0.0-beta.1

--- packages/ionic-angular/src/utils/versions.ts
+++ packages/ionic-angular/src/utils/versions.ts
@@ -1,4 +1,4 @@
 export const nxVersion = '^12.0.0';
-export const nxtendCapacitorVersion = '^12.0.0-beta.0';
+export const nxtendCapacitorVersion = '^12.0.0-beta.1';
 export const ionicAngularVersion = '^5.8.3';
 export const ionicNativeVersion = '^5.30.0';
