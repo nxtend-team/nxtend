b3adc4ef62176778325a1fd5ca7be77d77d866a7
Eduardo Roth <eduardo@eduardoroth.dev>
2021-11-16 00:42:00 -0600

    chore(): updated versions for react package

--- packages/ionic-react/src/utils/versions.ts
+++ packages/ionic-react/src/utils/versions.ts
@@ -1,5 +1,5 @@
-export const nxVersion = '^12.0.0';
-export const nxtendCapacitorVersion = '^12.0.0-beta.1';
+export const nxVersion = '^13.0.0';
+export const nxtendCapacitorVersion = '^12.0.0-beta.2';
 export const ionicReactVersion = '^5.8.3';
 export const ionicReactRouterVersion = '^5.8.3';
 export const ioniconsVersion = '^5.5.3';
