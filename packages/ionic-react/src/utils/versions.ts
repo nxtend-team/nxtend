dd462a9cbc53402c88f2109eacd507258c483f40
Devin Shoemaker <devinshoe@gmail.com>
2021-11-18 13:57:53 -0600

    chore(ionic-react): update @nxtend/capacitor to 12.0.0

--- packages/ionic-react/src/utils/versions.ts
+++ packages/ionic-react/src/utils/versions.ts
@@ -1,5 +1,5 @@
 export const nxVersion = '^12.0.0';
-export const nxtendCapacitorVersion = '^12.0.0-beta.1';
+export const nxtendCapacitorVersion = '^12.0.0';
 export const ionicReactVersion = '^5.8.3';
 export const ionicReactRouterVersion = '^5.8.3';
 export const ioniconsVersion = '^5.5.3';
