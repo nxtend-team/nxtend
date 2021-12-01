93f146cd119cf9a7e8a62d2cecba2179bed4fc8d
Devin Shoemaker <devinshoe@gmail.com>
2021-12-01 10:15:18 -0600

    chore(core): upgrade to Nx 13

--- packages/ionic-react/src/utils/versions.ts
+++ packages/ionic-react/src/utils/versions.ts
@@ -1,4 +1,4 @@
-export const nxVersion = '^12.0.0';
+export const nxVersion = '^13.0.0';
 export const nxtendCapacitorVersion = '^12.0.0';
 export const ionicReactVersion = '^5.8.3';
 export const ionicReactRouterVersion = '^5.8.3';
